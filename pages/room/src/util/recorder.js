6b8646a8503a16f265366cae166c321cac9001c5
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-27 14:33:07 -0300

    Create recorder.js

---
+++ pages/room/src/util/recorder.js
@@ -0,0 +1,49 @@
+class Recorder {
+    constructor(userName, stream) {
+        this.userName = userName
+        this.stream = stream
+        this.fileName = `id:${userName}-at:${Date.now()}`
+        this.videoType = "video/webm"
+
+        this.mediaRecorder = {}
+        this.recorderBlobs = []
+        this.completeRecordings = []
+        this.recordingActive = false
+    }
+    _setup() {
+        const commonCodecs = ["codecs=vp9,opus", "codecs=vp8,opus", '']
+        const options = commonCodecs
+            .map(codec => ({ mimetype: `${this.videoType};${codec}` }))
+            .find(options => MediaRecorder.isTypeSupported(options))
+        if (!options) {
+            throw new Error(`None of the codecs: ${commonCodecs.join(',')} are supported`)
+        }
+        return options
+    }
+    startRecording() {
+        const options = this._setup()
+        // Se não tiver recebendo stream
+        if (!this.stream.active) return;
+        this.mediaRecorder = new MediaRecorder(this.stream, options)
+        this.mediaRecorder.onstop = (event) => {
+
+        }
+        this.mediaRecorder.ondataavaliable = (event) => {
+            if (!event.data || !event.data.size) return;
+            this.recorderBlobs.push(event.data)
+
+        }
+        this.mediaRecorder.start()
+        this.recordingActive = true
+    }
+    async stopRecording() {
+        if (!this.recordingActive) return;
+        if (this.mediaRecorder.state === 'inactive') return;
+
+        this.mediaRecorder.stop()
+        this.recordingActive = false
+        await Util.sleep(200)
+        this.completeRecordings.push([...this.recorderBlobs])
+        this.recorderBlobs = []
+    }
+}
