6217d5391d41266bba69225f440f5a593bd6f88e
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-27 14:16:53 -0300

    Update peer.js

--- pages/room/src/util/peer.js
+++ pages/room/src/util/peer.js
@@ -2,11 +2,22 @@ class PeerBuilder {
     constructor({ peerConfig }) {
         this.peerConfig = peerConfig
 
-        const defaultFunctionValue = () => {}
+        const defaultFunctionValue = () => { }
         this.onError = defaultFunctionValue
         this.onCallReceived = defaultFunctionValue
         this.onConnectionOpened = defaultFunctionValue
         this.onPeerStreamReceived = defaultFunctionValue
+        this.onCallError = defaultFunctionValue
+        this.onCallClose = defaultFunctionValue
+    }
+    
+        setOnCallError(fn) {
+        this.onCallError = fn
+        return this
+    }
+       setOnCallClose(fn) {
+        this.onCallClose = fn
+        return this
     }
 
     setOnError(fn) {
@@ -31,16 +42,28 @@ class PeerBuilder {
 
     _prepareCallEvent(call) {
         call.on('stream', stream => this.onPeerStreamReceived(call, stream))
-
+        call.on('error', error => this.onCallError(call, error))
+        call.on('close', _ => this.onCallClose(call))
         this.onCallReceived(call)
+        
+     _preparePeerInstanceFunction(peerModule) {
+        class PeerCustomModule extends peerModule { }
+        const peerCall = PeerCustomModule.prototype.call
+        const context = this
+        PeerCustomModule.prototype.call = function (id, stream) {
+            const call = peerCall.apply(this, [id, stream])
+            
+            context._prepareCallEvent(call)
+            return call
+        }
+        return PeerCustomModule
     }
 
-    build() {
-        const peer = new Peer(...this.peerConfig)
-
+     build() {
+        const PeerCustomInstance = this._preparePeerInstanceFunction(Peer)
+        const peer = new PeerCustomInstance(...this.peerConfig)
         peer.on('error', this.onError)
         peer.on('call', this._prepareCallEvent.bind(this))
-
         return new Promise(resolve => peer.on('open', id => {
             this.onConnectionOpened(peer)
             return resolve(peer)
