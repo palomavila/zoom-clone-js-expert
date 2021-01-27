5080ea0299af9f74253790e37788ae9c10585de0
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-27 14:30:09 -0300

    Update app.js

--- pages/room/src/app.js
+++ pages/room/src/app.js
@@ -1,20 +1,10 @@
-const recordClick = function (recorderBtn) {
-  this.recordingEnabled = false;
-  return () => {
-    this.recordingEnabled = !this.recordingEnabled;
-    recorderBtn.style.color = this.recordingEnabled ? 'red' : 'white';
-  };
-};
-
 const onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const room = urlParams.get('room');
-  console.log('this is the room', room);
-
-  // const recorderBtn = document.getElementById('record')
-  // recorderBtn.addEventListener('click', recordClick(recorderBtn))
-
- const peerConfig = Object.values({
+  console.log('this is the room', room)
+  const socketUrl = 'http://localhost:3000'
+  const socketBuilder = new SocketBuilder({ socketUrl })
+  const peerConfig = Object.values({
     id: undefined,
     config: {
       port: 9000,
@@ -23,19 +13,12 @@ const onload = () => {
     }
   })
   const peerBuilder = new PeerBuilder({ peerConfig })
-
   const view = new View()
   const media = new Media()
   const deps = {
-    view,
-    media,
-    room,
-    socketBuilder,
-    peerBuilder
+    view, media, room, socketBuilder, peerBuilder
   }
-
   Business.initialize(deps)
-
 }
 
 window.onload = onload
