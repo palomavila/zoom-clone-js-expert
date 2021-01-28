349d2779f5e0e9325b7e8048602691d7a709a7fd
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-28 13:08:38 -0300

    Update recorder.js

--- pages/room/src/util/recorder.js
+++ pages/room/src/util/recorder.js
@@ -2,59 +2,62 @@ class Recorder {
     constructor(userName, stream) {
         this.userName = userName
         this.stream = stream
-        this.fileName = `id:${userName}-at:${Date.now()}`
-        this.videoType = "video/webm"
-
+        this.filename = `id:${userName}-when${Date.now()}`
+        this.videoType = 'video/webm'
         this.mediaRecorder = {}
-        this.recorderBlobs = []
+        this.recordedBlobs = []
         this.completeRecordings = []
         this.recordingActive = false
     }
+
     _setup() {
-        const commonCodecs = ["codecs=vp9,opus", "codecs=vp8,opus", '']
-        const options = commonCodecs
-            .map(codec => ({ mimetype: `${this.videoType};${codec}` }))
-            .find(options => MediaRecorder.isTypeSupported(options))
+        const commonCodecs = [
+            "codecs=vp9,opus",
+            "codecs=vp8,opus",
+            ""
+        ]
+
+        const options = commonCodecs.map(codec => ({ mimeType: `${this.videoType};${codec}` }))
+            .find(options => MediaRecorder.isTypeSupported(options.mimeType))
         if (!options) {
-            throw new Error(`None of the codecs: ${commonCodecs.join(',')} are supported`)
+            throw new Error(`nonce of the codecs:${commonCodecs.join(',')} are supported`)
+
         }
+
         return options
     }
+
     startRecording() {
         const options = this._setup()
-
-        if (!this.stream.active) return;
-        this.mediaRecorder = new MediaRecorder(this.stream, options)
-        console.log(`Created MediaRecorder ${this.mediaRecorder} with options ${options}`)
-        }
-    
+        if(!this.stream.active) return;
+        this.mediaRecorder = new MediaRecorder(this.stream,options)
+        console.log(`Created MediaRecorder ${this.mediaRecorder}with options ${options}`)
         this.mediaRecorder.onstop = (event) => {
-        console.log(`Recorded Blobs`, this.recordedBlobs)  
+            console.log('Recorded Blobs',this.recordedBlobs)
         }
-        
-        this.mediaRecorder.ondataavaliable = (event) => {
-            if (!event.data || !event.data.size) return;
-            this.recorderBlobs.push(event.data)
-            
+
+        this.mediaRecorder.ondataavailable = (event) =>{
+            if(!event.data || !event.data.size)return;
+
+            this.recordedBlobs.push(event.data)
         }
-        
         this.mediaRecorder.start()
-        console.log(`Media Recorder started, this.mediaRecorder)
+        console.log(`Media Recorted started`,this.mediaRecorder)
         this.recordingActive = true
     }
     
-    async stopRecording() {
-        if (!this.recordingActive) return;
-        if (this.mediaRecorder.state === 'inactive') return;
+    async stopRecording(){
+        if(!this.recordingActive)return
+        if(this.mediaRecorder.state === "inactive")return;
 
-        console.log('`media recorder stopped!`, this.userName)  
+        console.log('media recorded stopped',this.userName)
         this.mediaRecorder.stop()
-   
         this.recordingActive = false
         await Util.sleep(200)
-        this.completeRecordings.push([...this.recorderBlobs])
-        this.recorderBlobs = []
-  }
+        this.completeRecordings.push([...this.recordedBlobs])
+        this.recordedBlobs = []
+    }
+    
     getAllVideoURLs(){
        return this.completeRecordings.map(recording =>{
             const superBuffer = new Blob(recording,{type:this.videoType})
