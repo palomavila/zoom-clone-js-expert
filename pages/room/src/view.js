021a9b61bb28ed520709eea931feecd50eba42fe
Paloma V <62178655+palomavila@users.noreply.github.com>
2021-01-28 13:32:26 -0300

    Update view.js

--- pages/room/src/view.js
+++ pages/room/src/view.js
@@ -66,7 +66,7 @@ class View {
     onLeaveClick (command) {
         return async() => {
             command()
-            await Util.sleep(1000)
+            await Util.sleep(3000)
             window.location = '/pages/home'
         }
     }
